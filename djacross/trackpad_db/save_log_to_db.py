370c19c374b0596b2de7c3f5adf5074bf0305279
mzfa <mzfa@mzfa-Ubuntu-1404LTS>
2014-06-04 14:20:04 +0800

    add save_log_to_db.py
    Change-Id: Icee45b5ae62435aca5a5ee8df4f5f46fc1ecc536
    Signed-off-by: mzfa <mzfa@mzfa-Ubuntu-1404LTS>

---
+++ djacross/trackpad_db/save_log_to_db.py
@@ -0,0 +1,20 @@
+'''
+Created on Jun 4, 2014
+
+@author: mzfa
+'''
+from models import *
+
+
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
