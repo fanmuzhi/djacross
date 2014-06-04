0808ce7bf0d282077f78051b03fb6818052a057d
mzfa <mzfa@mzfa-Ubuntu-1404LTS>
2014-06-04 10:44:46 +0800

    trackpad_db_app added

--- djacross/djacross/settings.py
+++ djacross/djacross/settings.py
@@ -55,10 +55,21 @@ WSGI_APPLICATION = 'djacross.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'trackpad_database',
+        'USER': 'root',
+        'PASSWORD': '7u8i9o0p',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
